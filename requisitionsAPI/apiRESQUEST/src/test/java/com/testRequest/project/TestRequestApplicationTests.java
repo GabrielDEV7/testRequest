fdedb14789e413b05b40113d30121353e415b486
Gabriel <gabriel.menezdev@gmail.com>
2024-02-25 10:27:10 -0300

    frontEnd Final

---
+++ requisitionsAPI/apiRESQUEST/src/test/java/com/testRequest/project/TestRequestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testRequest.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestRequestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- requisitionsAPI/requisition.js
+++ requisitionsAPI/requisition.js
@@ -3,28 +3,50 @@ const firtsname = document.querySelector(".nameId");
 const lastname = document.querySelector(".lastnameId");
 const contact = document.querySelector(".contactId");
 const cep = document.querySelector("#cepId");
-const city  = document.querySelector("#cityId");
+const city = document.querySelector("#cityId");
 const state = document.querySelector("#stateId");
 const email = document.querySelector(".emailId");
 
+function cadastrar() {
+  fetch("http/localhost:8080/cadastrar", {
+    headers: {
+      Accept: "application/json",
+      "content-type": "application/json",
+    },
+    method: "POST",
+    body: JSON.stringify({
+      name: firtsname.value,
+      Lastname: lastname.value,
+      contact: contact.value,
+      cep: cep.value,
+      city: city.value,
+      state: state.value,
+      email: email.value,
+    }),
+  })
+    .then(function (res) {
+      console.log(res);
+    })
+    .catch(function (res) {
+      console.log(res);
+    });
+};
 
-formulario.addEventListener('submit', function (event){
-    event.preventDefault();
+function clear() {
+  firtsname.value = "";
+  lastname.value = "";
+  contact.value = "";
+  cep.value = "";
+  city.value = "";
+  state.value = "";
+  email.value = "";
+};
 
-    const dados = {
-        name: firtsname.value,
-        Lastname: lastname.value,
-        contact: contact.value,
-        cep: cep.value,
-        city: city.value,
-        state: state.value,
-        email: email.value
-    };
-    console.log(dados)
-}
-);
+formulario.addEventListener("submit", function (event) {
+  event.preventDefault();
 
-
-
+  cadastrar();
+  clear();
+});
 
 //https://viacep.com.br/
